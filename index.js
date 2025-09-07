f798884dcfb1bcbc5798bc4605f0541f3c5482cf
Sijun Liu <160683198+sliu369@users.noreply.github.com>
2025-09-07 18:13:49 -0400

    vite

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const client = new OpenAI({
     dangerouslyAllowBrowser: true,
-    apiKey: "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", // Use the API key from the environment variable
+    apiKey: import.meta.env.VITE_API_KEY, // Use the API key from the environment variable
 });
 
 document.querySelector(".translate-btn").addEventListener("click", async () => {
